c36c50631932f3bdd4c428585b38ca0dec9ebef4
Sufiyan33 <Basu700736@gmail.com>
2023-11-07 13:50:12 +0530

    Created new service named Restaurants-service

---
+++ Restaurants-Service/src/main/java/com/fast/food/restaurants/service/RestaurantsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fast.food.restaurants.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsServiceApplication.class, args);
+	}
+
+}
---
+++ Restaurants-Service/src/test/java/com/fast/food/restaurants/service/RestaurantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.food.restaurants.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
