f3fdb801ed09183d6cf1c89447de6298c1e648f8
Samir <attarsamir06@gmail.com>
2023-11-14 11:01:36 +0530

    Updated with skelton

---
+++ Cart-Service/src/main/java/com/fast/food/cart/service/config/CartConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fast.food.cart.service.config;
+
+/**
+ *
+ * @author 91976
+ */
+public class CartConfig {
+    
+}
---
+++ Cart-Service/src/test/java/com/fast/food/cart/service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.food.cart.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
